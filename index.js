b2f3ff24d358e7a90af47d9cc2fee921b3a446b1
groninge <groninge@gmail.com>
2024-09-04 10:32:02 +0200

    enable ng-apimock

---
+++ index.js
@@ -0,0 +1,19 @@
+const apimock = require('@ng-apimock/core')
+const devInterface = require('@ng-apimock/dev-interface')
+const express = require('express')
+const cors = require('cors')
+
+apimock.processor.process({
+  src: '',
+  //watch: true,
+})
+
+const app = express()
+app.use(cors())
+app.set('port', 9999)
+app.use('/dev-interface', express.static(devInterface))
+app.use(apimock.middleware)
+app.listen(app.get('port'), () => {
+  console.log('@ng-apimock/core running on port', app.get('port'))
+  console.log('@ng-apimock/dev-interface is available under /dev-interface')
+})
